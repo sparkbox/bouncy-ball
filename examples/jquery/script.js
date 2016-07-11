6c33ab67c42c903992707992d9928320ceaa44d8
Bryan Braun <bbraun7@gmail.com>
2016-07-11 06:39:06 -0400

    :art: style: update variable names to better indicate constants.

--- examples/jquery/script.js
+++ examples/jquery/script.js
@@ -1,16 +1,16 @@
 var $ball = $('ball'),
-    duration = 575;
+    DURATION = 575;
 
 function dropBall() {
   // We animate 'top' because jquery can't animate tranforms (wihtout plugins):
   // See https://bugs.jquery.com/ticket/4171
-  $ball.animate({ top: '160px' }, duration, 'easeInQuad', restoreBall);
+  $ball.animate({ top: '160px' }, DURATION, 'easeInQuad', restoreBall);
 }
 
 function restoreBall() {
   // Easings ('easeInQuad' & 'easeOutQuad') provided by jQuery UI.
   // See https://jqueryui.com/easing/
-  $ball.animate({ top: '0' }, duration, 'easeOutQuad', dropBall);
+  $ball.animate({ top: '0' }, DURATION, 'easeOutQuad', dropBall);
 }
 
 $(document).ready(function(){
