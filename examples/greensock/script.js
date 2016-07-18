90c777112a83175a8642c836315a17167647222b
Bryan Braun <bbraun7@gmail.com>
2016-07-18 18:10:54 -0400

    :art: fixes #1: simplify greensock code.

--- examples/greensock/script.js
+++ examples/greensock/script.js
@@ -1,7 +1,7 @@
 var ball = document.getElementsByTagName('ball');
 
 TweenMax.to(ball, 0.575, {
-  transform: 'translate3d(0px, 160px, 0px)',
+  y: 160,
   repeat: -1,
   yoyo: true,
   ease: Power1.easeIn
