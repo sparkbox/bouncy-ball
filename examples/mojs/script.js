f92e479f07cd40656236077655f4af5fa2e6ebd2
Oleg Solomka <legomushroom@gmail.com>
2016-08-28 07:15:57 -0500

    make the mojs example shorter

--- examples/mojs/script.js
+++ examples/mojs/script.js
@@ -1,13 +1,9 @@
 var ball = new mojs.Shape({
-  shape:    'circle',
-  fill:     '#613961',
-  radius:   25,
-  top:      33,
-  left:     33,
-  y:        { 0 : 160 },
-  duration: 575,
-  repeat:   1,
-  easing:   'quad.in',
-  isYoyo:   true,
-  onComplete () { ball.replay(); }
+  radius:    25,
+  y:         { '-80' : 80 },
+  duration:  575,
+  easing:    'quad.in',
+  isYoyo:    true,
+  repeat:    99999
 }).play();
+
