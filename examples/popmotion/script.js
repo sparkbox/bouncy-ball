d71bec0c20c6e7147ac855191d5d9f90f5cdccd0
Matt Perry <mattgperry@gmail.com>
2017-12-18 16:51:03 +0000

    calling cdn

--- examples/popmotion/script.js
+++
@@ -1,9 +0,0 @@
-const { styler, tween, easing } = popmotion;
-const ballStyler = styler(document.getElementById('ball'));
-
-tween({
-  to: 160,
-  duration: 575,
-  ease: easing.easeIn,
-  yoyo: Infinity
-}).start(ballStyler.set('y'));
