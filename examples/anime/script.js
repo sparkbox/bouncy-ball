1d26149a77f6377116763f1dbceb5c18c99a8cce
chriss <chriss@vandorensales.com>
2020-10-01 11:01:23 -0700

    Update script to use latest library and eslint standards

--- examples/anime/script.js
+++ examples/anime/script.js
@@ -1,23 +1,31 @@
-var bounceUp = anime({
-  autoplay: false,    //We don't want to immediately start the animation
-  targets:  '#ball',  //target the div '#ball'
-  translateY: {
-    value:    ['160px', '0px'], //When bouncing up, start at 160px and end at 0px
-    duration: 575,
-    easing:   'easeOutQuad',
-  },
-  complete:  function(){ bounceDown.play() }, //Function encapsulation is necessary, otherwise we run into not defined error
-});
+const { anime } = window;
 
-var bounceDown = anime({
-  autoplay: false,    //See similar comments above
-  targets:  '#ball',
-  translateY: {
-    value:    ['0px', '160px'], //When bouncing down, start at 0px and end at 160px
-    duration: 575,
-    easing:   'easeInQuad',
-  },
-  complete:  function(){ bounceUp.play() }, //After we bounce down, start the bounce up animation
-});
+function bounce() {
+  const bounceUp = anime({
+    autoplay: false, // We don't want to immediately start the animation
+    targets: '#ball', // target the div '#ball'
+    translateY: {
+      value: ['160px', '0px'], // When bouncing up, start at 160px and end at 0px
+      duration: 575,
+      easing: 'easeOutQuad',
+    },
+    complete: bounce,
+  });
 
-bounceDown.play(); //Start the animation!
+  const bounceDown = anime({
+    autoplay: false, // See similar comments above
+    targets: '#ball',
+    translateY: {
+      value: ['0px', '160px'], // When bouncing down, start at 0px and end at 160px
+      duration: 575,
+      easing: 'easeInQuad',
+    },
+    complete() {
+      bounceUp.play(); // After we bounce down, start the bounce up animation
+    },
+  });
+
+  bounceDown.play();
+}
+
+bounce(); // Start the animation!
