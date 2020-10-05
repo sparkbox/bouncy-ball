7967efb4ed0292c9fc677b2e145ae19bf15ad09d
Edgardo Ramírez <soldiercrp@gmail.com>
2020-10-05 13:23:59 -0500

    UPDATE: Bodymovin to latest version

--- examples/lottie/script.js
+++ examples/lottie/script.js
@@ -1,6 +1,6 @@
 // You can see the data that was exported from After Effects at
 // https://sparkbox.github.io/bouncy-ball/examples/lottie/data.json
-bodymovin.loadAnimation({
+lottie.loadAnimation({
   container: document.querySelector('ball'),
   renderer: 'svg',
   loop: true,
