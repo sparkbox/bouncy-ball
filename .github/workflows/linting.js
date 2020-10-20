84f96dd077e8a81a754230f5df8e381cd244f7fc
Bryan Braun <bryan@heysparkbox.com>
2020-10-20 10:53:08 -0400

    feat: add github action for automatic code linting

---
+++ .github/workflows/linting.js
@@ -0,0 +1,19 @@
+name: Run Linting
+
+on: [push]
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v2
+    - name: Use Node.js
+      uses: actions/setup-node@v1
+      with:
+        node-version: '12.x'
+    - name: Install devDependencies
+      run: npm ci
+    - name: Run Linting
+      run: npm test
