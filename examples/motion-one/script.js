5f8c3b960a14f7e0ad7ce0df7904d7754e9faa26
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-12-17 08:59:45 +0100

    fix: motion one demo

--- examples/motion-one/script.js
+++ examples/motion-one/script.js
@@ -2,11 +2,11 @@ const ball = document.getElementsByTagName("ball");
 
 Motion.animate(
   ball,
-  { y: [160, 0] },
+  { y: [0, 160] },
   {
     duration: .575,
     repeat: Infinity,
-    direction: "alternate-reverse",
-    ease: "cubic-bezier(.6, 0.08, 0.8, .6)"
-  },
+    direction: "alternate",
+    easing: "cubic-bezier(.6, 0.08, 0.8, .6)"
+  }
 );
