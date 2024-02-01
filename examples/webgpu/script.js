6bb6b222d0a91e7d825cbec1de849e3851289f77
Brandon Jones <tojiro@gmail.com>
2024-02-01 11:30:20 -0800

    Added more comments to the WebGPU example

--- examples/webgpu/script.js
+++ examples/webgpu/script.js
@@ -1,6 +1,8 @@
+// Requests a WebGPU adapter and create a device from it.
 const adapter = await navigator.gpu.requestAdapter();
 const device = await adapter.requestDevice();
 
+// Get a WebGPU context from the canvas and configure it for use with the device.
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('webgpu');
 context.configure({
@@ -12,6 +14,7 @@ context.configure({
 // Set up the vertex and fragment shaders.
 const module = device.createShaderModule({
   code: `
+    // Define the four corners of the square being rendered
     var<private> pos : array<vec2f, 4> = array<vec2f, 4>(
       vec2f(-1, -1),
       vec2f(1, -1),
@@ -19,6 +22,7 @@ const module = device.createShaderModule({
       vec2f(1, 1),
     );
 
+    // Define the uniform values that will be read from the uniformBuffer
     struct Uniforms {
       time: f32,
       h: f32,
@@ -28,6 +32,7 @@ const module = device.createShaderModule({
     }
     @group(0) @binding(0) var<uniform> u: Uniforms;
 
+    // Define the vertex shader outputs/fragment shader inputs
     struct VertexOut {
       @builtin(position) pos: vec4f,
       @location(0) center: vec2f,
@@ -36,23 +41,28 @@ const module = device.createShaderModule({
 
     @vertex
     fn vertexMain(@builtin(vertex_index) i: u32) -> VertexOut {
+      // Calculate the y position of the ball center
       let ypos = (u.a * pow((((u.time + u.h) % (u.h * 2.0)) - u.h), 2.0) + u.k) - u.k/2.0;
 
-      let circleSize = vec2f(100) / u.canvasSize;
-
       var out: VertexOut;
+
+      // Scale the ball size appropriately for the canvas
+      let circleSize = vec2f(100) / u.canvasSize;
       out.center = vec2f(0, ypos);
       out.worldPos = (pos[i] * circleSize) + out.center;
+
+      // Set the vertex positions in Normalized Device Coordinates
       out.pos = vec4f(out.worldPos, 0, 1);
       return out;
     }
 
     @fragment
     fn fragmentMain(in: VertexOut) -> @location(0) vec4f {
+      // Compute the distance from the center of the circle in pixels.
       let circleSize = vec2f(100) / u.canvasSize;
-
       let diff = (in.worldPos - in.center) / circleSize;
       let d = distance(diff, vec2f(0));
+
       if (d > 0.5) {
         discard; // Don't write to any pixels outside the circle.
       }
@@ -62,6 +72,7 @@ const module = device.createShaderModule({
   `
 });
 
+// Create a render pipeline that uses the above shader.
 const pipeline = device.createRenderPipeline({
   layout: 'auto',
   vertex: {
@@ -78,18 +89,19 @@ const pipeline = device.createRenderPipeline({
   }
 });
 
+// Create a buffer for the shader uniforms.
 const uniformBuffer = device.createBuffer({
   size: 8 * Float32Array.BYTES_PER_ELEMENT,
   usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
 });
-
-const uniformArray = new Float32Array(8);
 
 // Calculating variables for animation.
 const h = 575;
 const k = 2 - (2*(50/canvas.clientHeight));
 const a = -4 * k / Math.pow(h * 2, 2);
 
+// Prepare the animation variables to be copied to the uniformBuffer.
+const uniformArray = new Float32Array(8);
 uniformArray[0] = 0; // time
 uniformArray[1] = h;
 uniformArray[2] = k;
@@ -97,6 +109,7 @@ uniformArray[3] = a;
 uniformArray[4] = canvas.clientWidth;
 uniformArray[5] = canvas.clientHeight;
 
+// Create a bind group with the uniformBuffer.
 const bindGroup = device.createBindGroup({
   layout: pipeline.getBindGroupLayout(0),
   entries: [{
@@ -105,6 +118,7 @@ const bindGroup = device.createBindGroup({
   }]
 });
 
+// The main frame loop.
 let start, time;
 (function drawFrame(timestamp) {
   if (!start) { start = timestamp };
@@ -114,6 +128,7 @@ let start, time;
   uniformArray[0] = time;
   device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
 
+  // Begin a command encoder and start a render pass
   const encoder = device.createCommandEncoder();
   const renderPass = encoder.beginRenderPass({
     colorAttachments: [{
@@ -124,13 +139,15 @@ let start, time;
     }]
   });
 
+  // Draw 4 vertices with the pipeline and bind group created above
   renderPass.setPipeline(pipeline);
   renderPass.setBindGroup(0, bindGroup);
   renderPass.draw(4);
 
+  // End the render pass and submit the commands.
   renderPass.end();
-
   device.queue.submit([ encoder.finish() ]);
 
+  // Queue up the next animation frame.
   window.requestAnimationFrame(drawFrame);
 })(performance.now());
